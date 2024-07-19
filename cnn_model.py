16139f6ea021432169d599c5d87a9592b20a1df5
Kathrin Hälbich <kathrin.haelbich@iubh.de>
2024-07-19 21:39:12 +0200

    load test data

--- cnn_model.py
+++ cnn_model.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Standard Library Imports
-from config import FILE_PATH
+from config import FILE_PATH_TRAIN, FILE_PATH_TEST
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -15,24 +15,33 @@ import matplotlib.pyplot as plt
 
 # Download dataset here: https://www.kaggle.com/datasets/zalando-research/fashionmnist/data
 # Read training data file
-train_df = pd.read_csv(FILE_PATH, sep=',')
-
+train_df = pd.read_csv(FILE_PATH_TRAIN, sep=',')
 # Checking data structure
-# train_df.info()  # -->  60000 entries; 785 entries, label to pixel784; dataype = int
+train_df.info()  # -->  60000 entries; 785 entries, label to pixel784; dataype = int
 train_df.head()  # --> first column is the label data, the remaining columns are pixel data
+
+# Read testing data file
+test_df = pd.read_csv(FILE_PATH_TEST, sep=';', dtype={11: str})
+# Checking data structure
+test_df.info()  # -->  60000 entries; 785 entries, label to pixel784; dataype = int
+test_df.head()  # --> first column is the label data, the remaining columns are pixel data
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # PREPROCESSING DATA
 # ----------------------------------------------------------------------------------------------------------------------
 
-# split train data into x and y arrays where x represents image data and y represents labels
+# split train and test data into x and y arrays where x represents image data and y represents labels
 train_data = np.array(train_df, dtype='float32')
+test_data = np.array(test_df, dtype='float32')
 
 # rescale data from 0 to 1 by dividing by 255
 x_train = train_data[:, 1:]/255  # actual data; includes every row and pixel-columns without label and header info
 y_train = train_data[:, 0]  # label info; all rows and first column
+x_test = test_data[:, 1:]/255
+y_test = test_data[:, 0]
 
-# split into validation and training data
+# split training data into validation and training set
 x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
 # visualize some sample data
