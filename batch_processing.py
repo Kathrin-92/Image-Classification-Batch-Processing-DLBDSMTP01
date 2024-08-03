9761fd45720d88be5e42c29c4ad847a707ab636f
Kathrin Hälbich <kathrin.haelbich@iubh.de>
2024-08-03 12:09:09 +0200

    refactor to include functions and include handling of batch data

--- batch_processing.py
+++ batch_processing.py
@@ -1,35 +1,144 @@
 """
-load the CSV file, preprocess the data, and convert it to the required format.
+load a batch of CSV files (aka image data), preprocess the data, and convert it to the required format.
 Send a POST request to the FastAPI endpoint with the preprocessed data.
 Process the API response and save the predictions to a file or temporary storage.
 """
 
+# ----------------------------------------------------------------------------------------------------------------------
+# IMPORTS
+# ----------------------------------------------------------------------------------------------------------------------
+
+# standard library imports
+import os
+import json
 import requests
 import pandas as pd
 import numpy as np
-from config import FILE_PATH_BATCH
+from datetime import datetime
+
+# local imports
+from config import BASE_PATH_BATCH, PROCESSED_FILES_PATH, PREDICTIONS_PATH
 
 
-# load and preprocess batch data
-input_df = pd.read_csv(FILE_PATH_BATCH, sep=';')
-print("Input DataFrame head:\n", input_df.head())
+# ----------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS FOR LOADING A BATCH OF DATA
+# ----------------------------------------------------------------------------------------------------------------------
 
-x_input = input_df.iloc[:,:].values / 255.0
-x_input = x_input.astype(np.float32)
-print("Shape before reshape:", x_input.shape)
 
-x_input = x_input.tolist()  # Convert to list for JSON serialization; Data should be flattened into list[list[float]] for serialization and transmission.
-print("reshaping to JSON")
+def load_data(base_path_to_batch_data, files_already_processed):
+    # load and preprocess batch data;
+    # check if data was already processed before; read multiple files from temporary storage
+    image_data_list = []
+    filenames_processed = []
 
-# Send preprocessed data to the prediction API
-response = requests.post('http://127.0.0.1:8000/get_batch_prediction', json={"data": x_input})
+    for filename in os.listdir(base_path_to_batch_data):
+        if filename.endswith(".csv") and filename not in files_already_processed:
+            file_patch_batch = os.path.join(base_path_to_batch_data, filename)
+            input_data = pd.read_csv(file_patch_batch, sep=';')
+            image_data_list.append(input_data)
+            filenames_processed.append(filename)
+        else:
+            if not filename.endswith(".csv"):
+                print(f"File '{filename}' is not a .csv file and will be skipped.")
 
-if response.status_code == 200:
-    predictions = response.json()
-    print("response status 200")
+    if not filenames_processed:
+        print("No new data to process...!")
+        return pd.DataFrame(), filenames_processed
+
+    df_image_data = pd.concat(image_data_list)
+    return df_image_data, filenames_processed
+
+
+def load_processed_files(filepath):
+    # check if there is already a list of already processed files; load or create it
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as f:
+            return json.load(f)
+    else:
+        with open(filepath, 'w') as f:
+            json.dump([], f)
+        return []
+
+
+def save_processed_files(filepath, files_list):
+    # save new processed image data to list
+    with open(filepath, 'w') as f:
+        json.dump(files_list, f)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FUNCTION FOR PREPROCESSING LOADED BATCH DATA
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def preprocess_data(df_image_data):
+    # preprocess image data (resizing images, normalizing pixel values)
+    df_image_data = df_image_data.values / 255.0
+    df_image_data = df_image_data.astype(np.float32)
+    # convert to list for JSON serialization; data needs to be flattened into list[list[float]]
+    preprocessed_image_data = df_image_data.tolist()
+    return preprocessed_image_data
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FUNCTION FOR COMMUNICATION WITH API
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def get_batch_predictions(preprocessed_data):
+    # send batch to api
+    endpoint_url = 'http://127.0.0.1:8000/get_batch_prediction'
+    response = requests.post(endpoint_url, json={"data": preprocessed_data})
+
+    if response.status_code == 200:
+        predictions = response.json()
+        return predictions
+
+    else:
+        print(f"An error occurred! \nThe status code is: {response.status_code}\nError message: {response.text}")
+        return []
+
+
+def save_predictions(predictions, output_directory):
     # Save predictions to a file or temporary storage
     predictions_df = pd.DataFrame(predictions)
-    # add the class names here later on
-    predictions_df.to_csv('/Users/kathrinhalbich/PycharmProjects/Image-Classification-Batch-Processing-DLBDSMTP01/batch_prediction_results/results_1.py', index=False)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+    file_name = f"prediction_results_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+    file_path = os.path.join(output_directory, file_name)
+    predictions_df.to_csv(file_path, index=False)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FUNCTION FOR MAIN BATCH PROCESS
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def batch_process():
+    # load and handle batch of image data
+    print("Check whether new data is available and load it, if available...")
+    file_name_processed_files = 'processed_files.json'
+    file_path_processed_files = os.path.join(PROCESSED_FILES_PATH, file_name_processed_files)
+    files_already_processed = load_processed_files(file_path_processed_files)
+
+    df_image_data, filenames_processed = load_data(BASE_PATH_BATCH, files_already_processed)
+
+    if df_image_data.empty:
+        return
+    else:
+        files_already_processed.extend(filenames_processed)
+        save_processed_files(file_path_processed_files, files_already_processed)
+
+        # preprocess image data
+        print("Start preprocessing new image data...")
+        preprocessed_image_data = preprocess_data(df_image_data)
+
+        # send preprocessed data to the prediction API
+        predictions = get_batch_predictions(preprocessed_image_data)
+        if not predictions:
+            print("No predictions were returned.")
+        else:
+            print("Predictions successful! Save results...")
+            save_predictions(predictions, PREDICTIONS_PATH)
+
+
+if __name__ == "__main__":
+    batch_process()
