a96421d91f7d74d36b5711229514582253528a3f
Kathrin Hälbich <kathrin.haelbich@iubh.de>
2024-09-07 11:35:22 +0200

    adjust to handle filename in results and add comments and docstring docu

--- api/fastapi_app.py
+++ api/fastapi_app.py
@@ -1,3 +1,14 @@
+"""
+The logic for the API endpoint that can receive picture data, parse it, and deliver predictions is contained in this script.
+
+It manages the subsequent actions:
+1. A NumPy array is created from the input data.
+2. To conform to the CNN model's necessary input format, the array is reshaped.
+3. Utilizing the CNN model, predictions are created.
+4. For every input, the highest class and associated probability are retrieved.
+5. After mapping the predictions to the appropriate class names, they are given back.
+"""
+
 # ----------------------------------------------------------------------------------------------------------------------
 # IMPORTS
 # ----------------------------------------------------------------------------------------------------------------------
@@ -10,7 +21,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mlflow.pyfunc
 from mlflow.exceptions import MlflowException
-# import uvicorn
+# import uvicorn # for local run
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -33,6 +44,7 @@ except MlflowException as e:
 # START API AND DEFINE INPUT AND OUTPUT
 # ----------------------------------------------------------------------------------------------------------------------
 
+# for local run
 #def run_server():
 #    config = uvicorn.Config(app, host="0.0.0.0", port=8000, log_level="debug", reload=True)
 #    server = uvicorn.Server(config)
@@ -44,11 +56,12 @@ app = FastAPI(title="Image Classifier API")
 # define model for input data
 class InputData(BaseModel):
     data: list[list[float]]
-
+    filenames: list[str]
 
 # define model for output data
 class PredictionResult(BaseModel):
     input: int
+    filename: str
     most_likely_class_name: str
     most_likely_class_number: int
     corresponding_probability: float
@@ -61,7 +74,10 @@ class PredictionResult(BaseModel):
 # define the index route
 @app.get('/')
 async def index():
-    return {"Message": "This is a Welcome Message"}
+    return {
+        "Message": "Welcome to the Image Classification API! This API connects a Convolutional Neural Network (CNN) model to a batch process that returns predictions for the most likely class based on image input.",
+        "Instructions": "To explore the API's documentation and available endpoints, please visit: http://localhost:8000/docs"
+    }
 
 
 # define the health check route
@@ -83,7 +99,7 @@ async def predict(input_data: InputData):
         # make predictions
         predictions = cnn_model.predict(x_input)
 
-        # get highest class and probability
+        # get the highest class and probability
         highest_class_number = predictions.argmax(axis=1)
         corresponding_probability = predictions.max(axis=1)
         print(highest_class_number)
@@ -94,19 +110,17 @@ async def predict(input_data: InputData):
 
         results = [{
             "input": i + 1,
-            "most_likely_class_name": class_names[highest_class_number],
-            "most_likely_class_number": int(highest_class_number),
-            "corresponding_probability": float(corresponding_probability)
-        } for i,
-              (highest_class_number, corresponding_probability)
-              in enumerate(zip(highest_class_number, corresponding_probability))]
+            "filename": input_data.filenames[i],
+            "most_likely_class_name": class_names[highest_class_number[i]],
+            "most_likely_class_number": int(highest_class_number[i]),
+            "corresponding_probability": float(corresponding_probability[i])
+        } for i in range(len(highest_class_number))]
 
         return results
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error processing input data: {str(e)}")
-        # input validation in FastAPI to prevent malformed or malicious data from causing issues
 
-
+# for local run
 #if __name__ == "__main__":
 #   run_server()
